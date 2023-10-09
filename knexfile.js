5eed77b4a600d5fd85cc782dcf4860734bdb7a62
cherrydub <cherrydub@gmail.com>
2023-10-09 15:07:45 +0100

    more changes made for backend api

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,21 @@
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: process.env.POSTGRES_HOST,       // Update to use POSTGRES_HOST
-      user: process.env.POSTGRES_USER,       // Update to use POSTGRES_USER
+      host: process.env.POSTGRES_HOST, // Update to use POSTGRES_HOST
+      user: process.env.POSTGRES_USER, // Update to use POSTGRES_USER
       password: process.env.POSTGRES_PASSWORD, // Update to use POSTGRES_PASSWORD
       database: process.env.POSTGRES_DATABASE, // Update to use POSTGRES_DATABASE
-      port: process.env.POSTGRES_PORT || 5432,         // If provided by Vercel, update to use POSTGRES_PORT
+      // port: process.env.POSTGRES_PORT || 5432,         // If provided by Vercel, update to use POSTGRES_PORT
     },
     migrations: {
-      directory: './db/migrations',
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: './db/seeds',
+      directory: "./db/seeds",
     },
   },
 };
-
-
-
 
 // module.exports = {
 //   development: {
