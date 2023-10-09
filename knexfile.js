e5552c5cc521358bb382790370d87de841f27930
cherrydub <cherrydub@gmail.com>
2023-10-09 16:37:53 +0100

    more testing db_name

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
       host: process.env.DB_HOST,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.NAME,
+      database: process.env.DB_NAME,
       port: process.env.DB_PORT,
     },
     migrations: {
