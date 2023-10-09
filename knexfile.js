c265f6b6b25ad0f64810244427fabe078cd6f26d
cherrydub <cherrydub@gmail.com>
2023-10-09 16:21:30 +0100

    more testing

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,12 @@
-module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      host: process.env.POSTGRES_HOST, // Update to use POSTGRES_HOST
-      user: process.env.POSTGRES_USER, // Update to use POSTGRES_USER
-      password: process.env.POSTGRES_PASSWORD, // Update to use POSTGRES_PASSWORD
-      database: process.env.POSTGRES_DATABASE, // Update to use POSTGRES_DATABASE
-      // port: process.env.POSTGRES_PORT || 5432,         // If provided by Vercel, update to use POSTGRES_PORT
-    },
-    migrations: {
-      directory: "./db/migrations",
-    },
-    seeds: {
-      directory: "./db/seeds",
-    },
-  },
-};
-
 // module.exports = {
 //   development: {
 //     client: "pg",
 //     connection: {
-//       host: process.env.HOST,
-//       user: process.env.USERNAME,
-//       password: process.env.PASSWORD,
-//       database: "todoapp", // Specify your database name here
-//       port: process.env.DBPORT,
+//       host: process.env.POSTGRES_HOST, // Update to use POSTGRES_HOST
+//       user: process.env.POSTGRES_USER, // Update to use POSTGRES_USER
+//       password: process.env.POSTGRES_PASSWORD, // Update to use POSTGRES_PASSWORD
+//       database: process.env.POSTGRES_DATABASE, // Update to use POSTGRES_DATABASE
+//       port: 5432, // If provided by Vercel, update to use POSTGRES_PORT
 //     },
 //     migrations: {
 //       directory: "./db/migrations",
@@ -35,3 +16,22 @@ module.exports = {
 //     },
 //   },
 // };
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+};
